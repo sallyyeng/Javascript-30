ba196a56e9ce374e33d177c38af0245dd2b4c4b2
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-02-20 00:42:03 -0800

    Add close button and fix transitions

--- flex-panel-gallery/app.js
+++ flex-panel-gallery/app.js
@@ -1,23 +1,43 @@
 const panels = document.querySelectorAll('.panel');
-let lastPanel = false;
+const closeBtn = document.querySelector('.close-btn');
+
+const selectPanel = function(e) {
+  // add active panel and inactive panel
+  panels.forEach(panel => {
+    panel.classList.add(panel === this ? 'active' : 'inactive');
+  });
+
+  closeBtn.style.display = 'initial';
+};
+
+const resetPanels = function() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+    panel.classList.remove('inactive');
+  });
+
+  closeBtn.style.display = 'none';
+};
+
+// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('click', selectPanel));
+closeBtn.addEventListener('click', resetPanels);
 
 // stretches the panel
-const toggleOpen = function() {
-  // close every open panel
-  panels.forEach(panel => {
-    if (panel !== this && panel.classList.contains('open')) {
-      panel.classList.toggle('open');
-    }
-  });
-  this.classList.toggle('open');
-};
+// const toggleOpen = function() {
+//   // close every open panel
+//   panels.forEach(panel => {
+//     if (panel !== this && panel.classList.contains('open')) {
+//       panel.classList.toggle('open');
+//     }
+//   });
+//   this.classList.toggle('open');
+// };
 
-// brings words back into the page
-const toggleActive = function(e) {
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('open-active');
-  }
-};
-
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+// // brings words back into the page
 // panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+// const toggleActive = function(e) {
+//   if (e.propertyName.includes('flex')) {
+//     this.classList.toggle('open-active');
+//   }
+// };
