34f4490a07f2ef11a5ccb5ccafa3de64e36f4d68
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2018-02-20 18:42:31 -0800

    Complete exercise

--- canvas/app.js
+++ canvas/app.js
@@ -7,28 +7,38 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#AAA';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 50;
+ctx.lineWidth = 1;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = Math.floor(Math.random() * 360) + 1;
 
 let draw = function(e) {
+
   if (!isDrawing) {
+    hue = Math.floor(Math.random() * 360) + 1;
+    ctx.lineWidth = 1;
     return;
   }
-  console.log(e);
+
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
   ctx.beginPath();
   // start from
   ctx.moveTo(lastX, lastY);
   // go to
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 
-  [lastX, lastY] = [e.offsetX, e.offsetY]
+  // change hue color and lineWidth with each movement
+  hue++;
+  ctx.lineWidth = ctx.lineWidth < 12 ? ctx.lineWidth += 0.25 : ctx.lineWidth;
 };
 
 canvas.addEventListener('mousemove', draw);
+
 // required b/c w/o it, the mouse down is still activated until
 // clicked again, even if you mouse up after mouse out
 canvas.addEventListener('mouseout', () => isDrawing = false);
@@ -37,4 +47,5 @@ canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY];
 });
+
 canvas.addEventListener('mouseup', () => isDrawing = false );
